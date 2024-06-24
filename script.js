f20296b6dc266ca8561a41cbca66377b19158175
Ishan <ishanjaiiswal.com>
2024-06-24 13:12:38 +0700

    category functionality

--- script.js
+++ script.js
@@ -10,6 +10,21 @@ const functioning = () => {
   let b1 = b * b;
   let resulting = a / b1;
   results.innerHTML = resulting;
+  if (resulting <= 18.5) {
+    xyz.innerHTML = "Normal";
+  } else if (resulting >= 18.5 && resulting <= 24.9) {
+    xyz.innerHTML = "Normal Range";
+  } else if (resulting >= 25.0 && resulting <= 29.9) {
+    xyz.innerHTML = "Overweight";
+  } else if (resulting >= 30.0) {
+    xyz.innerHTML = "Obese";
+  } else if (resulting >= 30.0 && resulting <= 34.9) {
+    xyz.innerHTML = "Obese Class I";
+  } else if (resulting >= 35.0 && resulting <= 39.9) {
+    xyz.innerHTML = "Obese Class II";
+  } else if (resulting >= 40) {
+    xyz.innerHTML = "Obese Class III";
+  }
 };
 
 submission.addEventListener("click", functioning);
